e398bc3047cb2288236ef8be281825b58b0d98e9
Jaspin Suji <gjasphinsuji@gmail.com>
2023-02-06 10:12:49 +0530

    Login Design Commit

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,8 @@
-import LoginForm from "../login/Signin/SigninForm";
+// import LoginForm from "../login/Signin/SigninForm";
+import RegisterForm from "../login/Signup/SignupForm";
 
-const Login = () => <LoginForm/>;
-export default Login;
+// const Login = () => <LoginForm/>;
+// export default Login;
+
+const Register =() => <RegisterForm/>
+export default Register;
